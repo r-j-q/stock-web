0f414df3eb49cc31be388a1b08c613fb29319b2b
3051813192@qq.com <3051813192@qq.com>
2022-08-26 22:22:54 +0800

    1

--- login/assets/js/login5.js
+++ login/assets/js/login5.js
@@ -6,102 +6,102 @@
   var tokens = JSON.parse(localStorage.getItem("userInfo")) || "";
   
   //下面是正式环境
-//   const stripe = Stripe("pk_live_51L8IpsIzNzEUKhl8uXHCShyfEXTUAcetBsKbaG690FCXWGcBlFWoxbRdqsKjvKjh2k1WSkRKXYhRl3iZVqLjwZSI00fmzBBSTm");
-//   const items = [{ id: "prod_LxQP3nkuvcykMZ" }];
+  const stripe = Stripe("pk_live_51L8IpsIzNzEUKhl8uXHCShyfEXTUAcetBsKbaG690FCXWGcBlFWoxbRdqsKjvKjh2k1WSkRKXYhRl3iZVqLjwZSI00fmzBBSTm");
+  const items = [{ id: "prod_LxQP3nkuvcykMZ" }];
  
-//  let elements;
+ let elements;
   
-//  async function initialize(goods_id) { 
-//   const response = await fetch(`${baseUrl}/user/order/create?paytype=stripe&goods_id=${goods_id}&payway=0`, {
-//      method: "get",
-//      headers: { "Content-Type": "application/json" ,
-//      Authorization: `Bearer ${tokens.token}`,
+ async function initialize(goods_id) { 
+  const response = await fetch(`${baseUrl}/user/order/create?paytype=stripe&goods_id=${goods_id}&payway=0`, {
+     method: "get",
+     headers: { "Content-Type": "application/json" ,
+     Authorization: `Bearer ${tokens.token}`,
     
-//     }, 
-//    }) 
-//    const { clientSecret } = await response.json();
+    }, 
+   }) 
+   const { clientSecret } = await response.json();
  
-//    const appearance = {
-//      theme: 'stripe',
-//    };
-//    console.log("====>",clientSecret)
-//    elements = stripe.elements({ appearance, clientSecret });
+   const appearance = {
+     theme: 'stripe',
+   };
+   console.log("====>",clientSecret)
+   elements = stripe.elements({ appearance, clientSecret });
  
-//    const paymentElement = elements.create("payment");
-//    paymentElement.mount("#payment-element");
-//  }
+   const paymentElement = elements.create("payment");
+   paymentElement.mount("#payment-element");
+ }
  
-//  async function handleSubmit(e) {
-//    e.preventDefault();
-//    setLoading(true);
+ async function handleSubmit(e) {
+   e.preventDefault();
+   setLoading(true);
  
-//    const { error } = await stripe.confirmPayment({
-//      elements,
-//      confirmParams: { 
-//        return_url: `${baseUrl}/pay/callbackstripe`,
-//      },
-//    });
+   const { error } = await stripe.confirmPayment({
+     elements,
+     confirmParams: { 
+       return_url: `${baseUrl}/pay/callbackstripe`,
+     },
+   });
   
-//    if (error.type === "card_error" || error.type === "validation_error") {
-//      showMessage(error.message);
-//    } else {
-//      showMessage("An unexpected error occurred.");
-//    }
+   if (error.type === "card_error" || error.type === "validation_error") {
+     showMessage(error.message);
+   } else {
+     showMessage("An unexpected error occurred.");
+   }
  
-//    setLoading(false);
-//  }
+   setLoading(false);
+ }
   
-//  async function checkStatus() {
-//    const clientSecret = new URLSearchParams(window.location.search).get(
-//      "payment_intent_client_secret"
-//    );
+ async function checkStatus() {
+   const clientSecret = new URLSearchParams(window.location.search).get(
+     "payment_intent_client_secret"
+   );
  
-//    if (!clientSecret) {
-//      return;
-//    }
+   if (!clientSecret) {
+     return;
+   }
  
-//    const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
+   const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
  
-//    switch (paymentIntent.status) {
-//      case "succeeded":
-//        showMessage("Payment succeeded!");
-//        break;
-//      case "processing":
-//        showMessage("Your payment is processing.");
-//        break;
-//      case "requires_payment_method":
-//        showMessage("Your payment was not successful, please try again.");
-//        break;
-//      default:
-//        showMessage("Something went wrong.");
-//        break;
-//    }
-//  }
+   switch (paymentIntent.status) {
+     case "succeeded":
+       showMessage("Payment succeeded!");
+       break;
+     case "processing":
+       showMessage("Your payment is processing.");
+       break;
+     case "requires_payment_method":
+       showMessage("Your payment was not successful, please try again.");
+       break;
+     default:
+       showMessage("Something went wrong.");
+       break;
+   }
+ }
   
  
-//  function showMessage(messageText) {
-//    const messageContainer = document.querySelector("#payment-message");
+ function showMessage(messageText) {
+   const messageContainer = document.querySelector("#payment-message");
  
-//    messageContainer.classList.remove("hidden");
-//    messageContainer.textContent = messageText;
+   messageContainer.classList.remove("hidden");
+   messageContainer.textContent = messageText;
  
-//    setTimeout(function () {
-//      messageContainer.classList.add("hidden");
-//      messageText.textContent = "";
-//    }, 4000);
-//  }
+   setTimeout(function () {
+     messageContainer.classList.add("hidden");
+     messageText.textContent = "";
+   }, 4000);
+ }
   
-//  function setLoading(isLoading) {
-//    if (isLoading) { 
-//      document.querySelector("#submit").disabled = true;
-//      document.querySelector("#spinner").classList.remove("hidden");
-//      document.querySelector("#button-text").classList.add("hidden");
-//    } else {
-//      document.querySelector("#submit").disabled = false;
-//      document.querySelector("#spinner").classList.add("hidden");
-//      document.querySelector("#button-text").classList.remove("hidden");
-//    }
-//  }
+ function setLoading(isLoading) {
+   if (isLoading) { 
+     document.querySelector("#submit").disabled = true;
+     document.querySelector("#spinner").classList.remove("hidden");
+     document.querySelector("#button-text").classList.add("hidden");
+   } else {
+     document.querySelector("#submit").disabled = false;
+     document.querySelector("#spinner").classList.add("hidden");
+     document.querySelector("#button-text").classList.remove("hidden");
+   }
+ }
   // stripe 支付模块
 
   // var baseUrl = "http://192.168.1.20:8080";
@@ -267,7 +267,7 @@
     $("#pay-paypel").hide()
     $("#pay-stripe").hide() 
     $("#payment-form").show()
-    // initialize(goods_id) ;
+    initialize(goods_id) ;
      
     checkStatus();
     document
